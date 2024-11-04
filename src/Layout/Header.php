850d384afd0aafe1bddf6b31cd8f144e8ca4f083
tomengsanchez <mrtomy31@gmail.com>
2024-11-04 11:11:41 +0800

    a dfs sfsf sdf sdf

--- src/Layout/Header.php
+++ src/Layout/Header.php
@@ -8,20 +8,52 @@ class Header
 
 
         ?>
+
+        <!-- Option 1: Include in HTML -->
+
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link active" href="?page=meter-reading-dashboard">Home</a>
+                        <?php
+                        $links = [
+                            'Home' => [
+                                'link' => '?page=meter-reading-dashboard',
+                                'icon' => 'bi-bar-chart-line'
+                            ],
+                            'Company' => [
+                                'link' => '?page=meter-reading-company',
+                                'icon' => 'bi-building'
+                            ],
+                            'Meters' => [
+                                'link' => '?page=meter-reading-company',
+                                'icon' => 'bi-speedometer2',
+                            ],
+                            'Departments' => [
+                                'link' => '?page=meter-reading-company',
+                                'icon' => 'bi-people-fill'
+                            ],
+                        ];
+
+                        foreach ($links as $index => $link) {
+                            
+                            ?>
+                            <a class="nav-link" href="?page=<?= $link['link'] ?>"><i
+                                    class="bi <?= $link['icon'] ?>"></i> <?= $index ?></a>
+                            <?php
+                        }
+                        ?>
+
+                        <!-- <a class="nav-link active" href="?page=meter-reading-dashboard">Home</a>
                         <a class="nav-link" href="?page=meter-reading-company">Company</a>
                         <a class="nav-link" href="?page=meter-reading-meters">Meters</a>
-                        <a class="nav-link" href="?page=meter-reading-departments">Departments</a>
+                        <a class="nav-link" href="?page=meter-reading-departments">Departments</a> -->
                     </div>
                 </div>
             </div>
         </nav>
 
         <div class="p-4">
-        <?php
+            <?php
     }
 }
